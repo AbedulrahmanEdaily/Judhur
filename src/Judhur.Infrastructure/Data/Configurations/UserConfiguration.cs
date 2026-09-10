144e70988b1029142607657f3d02fcf0d11d69d0
Abdulrahman Edaily <dylyb7883@gmail.com>
2026-09-10 23:08:21 +0300

    feat(EF-Configuration):add configuration for user

---
+++ src/Judhur.Infrastructure/Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,27 @@
+using Judhur.Domain.Users;
+using Judhur.Infrastructure.Identity;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Judhur.Infrastructure.Data.Configurations;
+
+public sealed class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasOne<ApplicationUser>()
+            .WithOne()
+            .HasForeignKey<User>(u => u.Id)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Property(u => u.Name).HasMaxLength(User.MaxNameLength);
+        builder.Property(u => u.PhoneNumber).HasMaxLength(User.MaxPhoneNumberLength);
+        builder.Property(u => u.BannedReason).HasMaxLength(User.MaxBanReasonLength);
+
+        builder.Property(u => u.Role).HasConversion<string>().HasMaxLength(32);
+
+        builder.HasIndex(u => u.Role);
+        builder.HasIndex(u => u.IsBanned);
+    }
+}
