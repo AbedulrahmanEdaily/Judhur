8ca16700072731daade9122e2f84088a6cd34814
Abdulrahman Edaily <dylyb7883@gmail.com>
2026-09-14 22:28:44 +0300

    feat(MediatR-pipeline):implement unhandledExceptionBehavior

--- src/Judhur.Application/DependencyInjection.cs
+++ src/Judhur.Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
-
+using System.Reflection;
+using FluentValidation;
+using Judhur.Application.Common.Behaviors;
 namespace Microsoft.Extensions.DependencyInjection;
 
 
@@ -6,6 +8,12 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddMediatR(options =>
+        {
+            options.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            options.AddOpenBehavior(typeof(UnhandledExceptionBehavior<,>));
+        });
         return services;
     }
 }
